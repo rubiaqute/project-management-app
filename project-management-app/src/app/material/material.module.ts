51a9a5fb7ee33defd60bda985759d42732d24a9a
rubiaqute <rubiaqute@gmail.com>
2022-04-26 00:14:19 +0300

    feat: add material module and custom theme

---
+++ project-management-app/src/app/material/material.module.ts
@@ -0,0 +1,30 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatButtonModule } from '@angular/material/button';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
+import { MatListModule } from '@angular/material/list';
+
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    MatSidenavModule,
+    MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSlideToggleModule,
+    MatCardModule,
+    MatDividerModule,
+    MatListModule,
+  ]
+})
+export class MaterialModule { }
