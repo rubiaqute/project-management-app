218fdd86a6904f490b1f76ee5308ab032af589dc
batashofa <batashofa@gmail.com>
2022-04-29 17:55:53 +0300

    feat: add service for auth, created api-services

--- project-management-app/src/app/core/models/auth.models.ts
+++
@@ -1,4 +0,0 @@
-export interface IAuth {
-  login: string,
-  password: string,
-}
--- project-management-app/src/app/core/models/board.models.ts
+++
@@ -1,7 +0,0 @@
-import {IColumn} from "./column.models";
-
-export interface IBoard {
-  id: string,
-  title: string,
-  columns?: IColumn[],
-}
