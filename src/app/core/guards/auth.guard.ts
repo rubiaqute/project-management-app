441dfef0de033c1ee5b024d0cdafb25b330d061a
rubiaqute <rubiaqute@gmail.com>
2022-05-03 03:05:49 +0300

    feat: implement guard

--- src/app/core/guards/auth.guard.ts
+++ src/app/core/guards/auth.guard.ts
@@ -2,21 +2,30 @@ import { Injectable } from '@angular/core';
 import {
   ActivatedRouteSnapshot,
   CanActivate,
+  CanActivateChild,
   CanLoad, Route,
   Router,
   RouterStateSnapshot,
   UrlSegment,
   UrlTree
 } from '@angular/router';
-import { Observable } from 'rxjs';
-import {AuthService} from "../../auth/services/auth.service";
+import { Store } from '@ngrx/store';
+import { map, Observable } from 'rxjs';
+import { MainState } from 'src/app/store/store';
+import { AuthService } from "../../auth/services/auth.service";
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuard implements CanActivate, CanLoad {
+export class AuthGuard implements CanActivate, CanActivateChild {
   constructor(private router: Router,
-              private readonly authService: AuthService) {}
+    private readonly authService: AuthService,
+    private store: Store<MainState>) { }
+
+  canActivateChild(route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    return this.handle();
+  }
 
   canActivate(
     route: ActivatedRouteSnapshot,
@@ -24,14 +33,8 @@ export class AuthGuard implements CanActivate, CanLoad {
     return this.handle();
   }
 
-  canLoad(
-    route: Route,
-    segments: UrlSegment[]): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.handle();
-  }
-
   handle() {
-    const isNavigationAllowed = this.authService.loadInfo();
-    return isNavigationAllowed || this.router.parseUrl('/auth/login');
+    const isAuthorized = localStorage.getItem('tokenRubiaqute')
+    return !!isAuthorized || this.router.parseUrl('auth/login')
   }
 }
